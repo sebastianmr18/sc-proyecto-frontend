a2c0d6353337933f7061bf649a14d078f6a2bfe2
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2024-11-04 11:28:52 -0500

    fix/eslint errors

--- app/auth/users/activation-pending/page.tsx
+++ app/auth/users/activation-pending/page.tsx
@@ -9,7 +9,7 @@ const ActivationPending = () => {
                 Thanks for registering! Please check your email for the activation link to activate your account.
             </p>
             <p>
-                If you didn't receive an email, you can <Link href="/auth/users/resend-activation">resend the activation email</Link>.
+                If you didn&apos;t receive an email, you can <Link href="/auth/users/resend-activation">resend the activation email</Link>.
             </p>
         </div>
     );
