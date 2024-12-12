5cac292d22de5b3f78a76c8ffbdfabd6c245b323
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2024-12-12 10:54:25 -0500

    feat: change welcome message

--- app/home/components/HeroSection.tsx
+++ app/home/components/HeroSection.tsx
@@ -7,16 +7,16 @@ const HeroSection = () => {
     return (
         <section className="bg-neutral-400 text-center p-[60px]">
             <div className="text-[2.5rem] mb-4">
-                <h1>Bienvenido a Nuestra Plataforma</h1>
+                <h1>Bienvenido al mejor alquiler de canchas de Cali</h1>
                 <p
                     className='text-[1.2rem] mb-8'
-                >Conecta con nuestras funciones y encuentra el servicio perfecto para ti.</p>
+                >Reserva tu cancha para ti y tu equipo de forma sencilla. Paga en nuestra pagina o en la cancha, tu eliges!</p>
                 <button 
                     onClick={() => 
-                        router.push('/register')
+                        router.push('/courts')
                     }
-                    className='text-base px-5 py-2.5 hover:bg-neutral-300 rounded-lg'
-                    >Explora ahora</button>
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+                    >Explora nuestas canchas disponibles</button>
             </div>
         </section>
     );
